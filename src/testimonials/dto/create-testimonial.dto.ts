3d266ecb65dc978e97a5b8bc423d4ba416b0ee3f
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-07-31 11:15:02 -0300

    feat: :sparkles: add: validation on testimonials incoming data

--- src/testimonials/dto/create-testimonial.dto.ts
+++ src/testimonials/dto/create-testimonial.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTestimonialDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   photo: string;
+
+  @IsNotEmpty()
+  @IsString()
   testimonial: string;
 }
