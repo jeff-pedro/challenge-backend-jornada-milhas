b232e639fbe16206fa9eb40e23c89b75b02a5e41
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-08-05 12:08:01 -0300

    feat: :sparkles: creates custom configuration to the envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DestinationsModule } from './destinations/destinations.module';
 import TestimonialsModule from './testimonials/testimonials.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [TestimonialsModule, DestinationsModule, ConfigModule.forRoot()],
+  imports: [
+    TestimonialsModule,
+    DestinationsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
 })
 export class AppModule {}
--- src/destinations/destinations.module.ts
+++ src/destinations/destinations.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DestinationsService } from './destinations.service';
 import { DestinationsController } from './destinations.controller';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [DestinationsController],
   providers: [DestinationsService],
-  imports: [ConfigModule.forRoot()],
 })
 export class DestinationsModule {}
