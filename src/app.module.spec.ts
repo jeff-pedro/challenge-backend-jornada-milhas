b086e019a042cfaaa4fc52c3b685d715b0bdd2e3
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-08-05 21:18:50 -0300

    test: :test_tube: adds tests for AppModule and destination service

---
+++ src/app.module.spec.ts
@@ -0,0 +1,27 @@
+import { Test } from '@nestjs/testing';
+import { AppModule } from './app.module';
+import { validate } from './validations/env.validation';
+
+describe('AppModule', () => {
+  beforeEach(async () => {
+    await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+  });
+
+  it('should validate environment variables', () => {
+    process.env.NODE_ENV = 'development';
+    process.env.PORT = '3000';
+    process.env.COHERE_API_KEY = 'test-api-key';
+
+    expect(() => validate(process.env)).not.toThrow();
+  });
+
+  it('should throw an error for invalid environment variables', () => {
+    process.env.NODE_ENV = 'invalid_env';
+    process.env.PORT = '-1';
+    process.env.COHERE_API_KEY = '';
+
+    expect(() => validate(process.env)).toThrow();
+  });
+});
