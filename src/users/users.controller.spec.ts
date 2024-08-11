b668b3d0c78b8e7a1c6f9b3c6f98e209f1763cfc
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-08-11 14:35:16 -0300

    test: :test_tube: tests for controller and user service

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -4,17 +4,28 @@ import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should usersService be defined', () => {
+    expect(usersService).toBeDefined();
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: Repository<User>;
+
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(User);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(USER_REPOSITORY_TOKEN);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('userRepository should be defined', () => {
+    expect(userRepository).toBeDefined();
   });
 });
