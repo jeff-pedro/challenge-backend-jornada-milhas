f720210824cb0a395be637a397e0f8a9b2b8b6c6
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-08-15 21:19:17 -0300

    fix: :bug: excludes propriety photos to update a destination

--- src/destinations/dto/update-destination.dto.ts
+++ src/destinations/dto/update-destination.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateDestinationDto } from './create-destination.dto';
 
-export class UpdateDestinationDto extends PartialType(CreateDestinationDto) {}
+export class UpdateDestinationDto extends PartialType(
+  OmitType(CreateDestinationDto, ['photos']),
+) {}
