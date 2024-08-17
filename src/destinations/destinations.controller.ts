7bc8bd477db52b32027baf91e97354c2d8796eba
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-08-16 21:45:09 -0300

    refactor: :recycle: refactor code by add a return type to the controller

--- src/destinations/destinations.controller.ts
+++ src/destinations/destinations.controller.ts
@@ -7,31 +7,37 @@ import {
   Param,
   Delete,
   Query,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { DestinationsService } from './destinations.service';
 import { CreateDestinationDto } from './dto/create-destination.dto';
 import { UpdateDestinationDto } from './dto/update-destination.dto';
 import { ListDestinationDto } from './dto/list-destination.dto';
+import { Destination } from './entities/destination.entity';
 
 @Controller('destinations')
 export class DestinationsController {
   constructor(private readonly destinationsService: DestinationsService) {}
 
   @Post()
-  async create(@Body() createDestinationDto: CreateDestinationDto) {
+  async create(
+    @Body() createDestinationDto: CreateDestinationDto,
+  ): Promise<{ data: Destination }> {
     const destination =
       await this.destinationsService.create(createDestinationDto);
     return { data: destination };
   }
 
   @Get()
-  async findAll(@Query('name') name: string) {
+  async findAll(@Query('name') name: string): Promise<{ data: Destination[] }> {
     const destinations = await this.destinationsService.findAll(name);
     return { data: destinations };
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  async findOne(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<{ data: ListDestinationDto }> {
     const destination = await this.destinationsService.findOne(id);
 
     return {
@@ -47,15 +53,17 @@ export class DestinationsController {
 
   @Patch(':id')
   async update(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateDestinationDto: UpdateDestinationDto,
-  ) {
+  ): Promise<{ message: string }> {
     await this.destinationsService.update(id, updateDestinationDto);
     return { message: `Destination #${id} was updated` };
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  async remove(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<{ message: string }> {
     await this.destinationsService.remove(id);
     return { message: `Destination #${id} was deleted` };
   }
