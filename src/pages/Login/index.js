958b9f4b7e1a508e63c95f1b30dfa9da4ef179a7
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-06-11 15:02:25 -0300

    add login page

---
+++ src/pages/Login/index.js
@@ -0,0 +1,7 @@
+import styles from './Logn.module.css';
+
+const Login = () => {
+  return(<h1>Login page</h1>)
+}
+
+export default Login;
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import Register from 'pages/Register';
 // components
 import Header from 'components/layout/Header';
 import Footer from 'components/layout/Footer';
+import Login from 'pages/Login';
 
 function AppRoutes() {
   return (
@@ -17,6 +18,7 @@ function AppRoutes() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
         <Route path='destinations/:id' element={<Destination />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
