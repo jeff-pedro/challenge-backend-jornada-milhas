db315e7700b6f19e874cd3fe59fe33fea744c40f
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-07-02 12:32:08 -0300

    feat: implement pagination for testimonials and improve carousel functionality

--- src/components/testimonials/TestimonialSection/index.js
+++ src/components/testimonials/TestimonialSection/index.js
@@ -2,15 +2,19 @@ import styles from './TestimonialSection.module.css';
 import Title from 'components/ui/Title';
 import TestimonialCarousel from 'components/testimonials/TestimonalCarousel';
 
-const TestimonialSection = ({ testimonials }) => {
+const TestimonialSection = ({ testimonials, loadPage, currentPage }) => {
   return(
     <>
-      {testimonials && testimonials.length > 0
+      {testimonials && testimonials.results.length > 0
         ?
           (
             <section className={styles.testimonials}>
               <Title>Depoimentos</Title>
-              <TestimonialCarousel testimonials={testimonials}/>
+              <TestimonialCarousel 
+                testimonials={testimonials}
+                loadPage={loadPage}
+                currentPage={currentPage}
+              />
             </section>
           )
         : ''
